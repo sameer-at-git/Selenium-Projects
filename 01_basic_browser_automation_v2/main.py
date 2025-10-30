d983b0cb8e5879ed27376817b01ff5be893d19c6
MD.Sameer Sayed <sameer0sayed@gmail.com>
2025-10-30 18:33:19 +0600

    v2 finished

---
+++ 01_basic_browser_automation_v2/main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver import By 
+browser = webdriver.Chrome()
+browser.get("http://aiub.edu/")
+#browser.quit()
+browser.maximize_window()
+title = browser.title
+print(title)
+
+#assert "AIUb" in title #assertion error
+assert "AIUB" in title
