f470cad5f23bfe62df688e2028ce3cb1d4430763
MD.Sameer Sayed <sameer0sayed@gmail.com>
2025-11-01 15:07:46 +0600

    Finished form Login

---
+++ 02_form_filling_and_login_bot/main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver=webdriver.Chrome()
+driver.get("https://practicetestautomation.com/practice-test-login/")
+time.sleep(2)
+driver.find_element(By.ID,"username").send_keys("student")
+driver.find_element(By.ID,"password").send_keys("Password123")
+driver.find_element(By.ID,"submit").click()
+time.sleep(3)
+driver.quit()
